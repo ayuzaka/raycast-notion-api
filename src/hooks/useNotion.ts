9e3469e3ea765e5f4f87d0e11085a76c9057a9d7
ayuzaka <ayuzaka@vivaldi.net>
2022-10-30 09:44:17 +0900

    refactor: set useCallback

--- src/hooks/useNotion.ts
+++ src/hooks/useNotion.ts
@@ -1,3 +1,4 @@
+import { useCallback } from "react";
 import { Client, APIResponseError } from "@notionhq/client";
 import type {
   CreatePageParameters,
@@ -48,15 +49,18 @@ type Property =
 export const useNotion = (auth: string, tagDatabaseId: string) => {
   const client = new Client({ auth });
 
-  const fetchProperties = async (pageId: string, propertyIds: string[]): Promise<GetPagePropertyResponse[]> => {
-    return await Promise.all(
-      propertyIds.map(
-        async (propertyId) => await client.pages.properties.retrieve({ page_id: pageId, property_id: propertyId })
-      )
-    );
-  };
+  const fetchProperties = useCallback(
+    async (pageId: string, propertyIds: string[]): Promise<GetPagePropertyResponse[]> => {
+      return await Promise.all(
+        propertyIds.map(
+          async (propertyId) => await client.pages.properties.retrieve({ page_id: pageId, property_id: propertyId })
+        )
+      );
+    },
+    [client.pages.properties]
+  );
 
-  const getIcon = (icon: PageObjectResponse["icon"]): string | null => {
+  const getIcon = useCallback((icon: PageObjectResponse["icon"]): string | null => {
     if (!icon) {
       return null;
     }
@@ -70,9 +74,9 @@ export const useNotion = (auth: string, tagDatabaseId: string) => {
     }
 
     return icon.file.url;
-  };
+  }, []);
 
-  const getProperty = (response: GetPagePropertyResponse): Property | null => {
+  const getProperty = useCallback((response: GetPagePropertyResponse): Property | null => {
     switch (response.type) {
       case "property_item": {
         const results = response.results;
@@ -94,9 +98,34 @@ export const useNotion = (auth: string, tagDatabaseId: string) => {
       default:
         return null;
     }
-  };
+  }, []);
 
-  const fetchTags = async (): Promise<readonly Tag[]> => {
+  const fetchDatabase = useCallback(
+    async (id: string): Promise<PageObjectResponse[]> => {
+      const allResults: PageObjectResponse[] = [];
+      let next: string | null = null;
+
+      do {
+        const parameter: QueryDatabaseParameters = {
+          database_id: id,
+          sorts: [{ property: "Name", direction: "ascending" }],
+        };
+
+        if (next) {
+          parameter.start_cursor = next;
+        }
+
+        const { results, next_cursor } = await client.databases.query(parameter);
+        allResults.push(...(results as PageObjectResponse[]));
+        next = next_cursor;
+      } while (next);
+
+      return allResults;
+    },
+    [client.databases]
+  );
+
+  const fetchTags = useCallback(async (): Promise<readonly Tag[]> => {
     const results = await fetchDatabase(tagDatabaseId);
     if (!results[0]) {
       return [];
@@ -127,155 +156,142 @@ export const useNotion = (auth: string, tagDatabaseId: string) => {
     );
 
     return tags;
-  };
+  }, [client.pages.properties, fetchDatabase, getIcon, getProperty, tagDatabaseId]);
 
-  const getBookmark = (id: string, responses: GetPagePropertyResponse[], favicon: string | null): Bookmark => {
-    const bookmark: Bookmark = {
-      id,
-      url: "",
-      name: "",
-      tag: [],
-      favicon,
-    };
-
-    responses.forEach((response) => {
-      const property = getProperty(response);
-      switch (property?.name) {
-        case "title":
-          bookmark.name = property.value;
-          break;
-
-        case "url":
-          bookmark.url = property.value;
-          break;
-
-        case "tag":
-          bookmark.tag.push(property.value);
-          break;
-
-        default:
-          break;
-      }
-    });
-
-    return bookmark;
-  };
-
-  const fetchDatabase = async (id: string): Promise<PageObjectResponse[]> => {
-    const allResults: PageObjectResponse[] = [];
-    let next: string | null = null;
-
-    do {
-      const parameter: QueryDatabaseParameters = {
-        database_id: id,
-        sorts: [{ property: "Name", direction: "ascending" }],
+  const getBookmark = useCallback(
+    (id: string, responses: GetPagePropertyResponse[], favicon: string | null): Bookmark => {
+      const bookmark: Bookmark = {
+        id,
+        url: "",
+        name: "",
+        tag: [],
+        favicon,
       };
 
-      if (next) {
-        parameter.start_cursor = next;
-      }
+      responses.forEach((response) => {
+        const property = getProperty(response);
+        switch (property?.name) {
+          case "title":
+            bookmark.name = property.value;
+            break;
 
-      const { results, next_cursor } = await client.databases.query(parameter);
-      allResults.push(...(results as PageObjectResponse[]));
-      next = next_cursor;
-    } while (next);
+          case "url":
+            bookmark.url = property.value;
+            break;
 
-    return allResults;
-  };
+          case "tag":
+            bookmark.tag.push(property.value);
+            break;
 
-  const stockArticle = async (databaseId: string, articleProps: Article): Promise<Result<string>> => {
-    const { title, url, ogp, icon, tags, published } = articleProps;
+          default:
+            break;
+        }
+      });
 
-    const parameters: CreatePageParameters = {
-      parent: { database_id: databaseId },
-      properties: {
-        Title: {
-          type: "title",
-          title: [{ text: { content: title } }],
-        },
-        URL: { type: "url", url },
-      },
-    };
+      return bookmark;
+    },
+    [getProperty]
+  );
 
-    if (icon) {
-      parameters.icon = {
-        type: "external",
-        external: {
-          url: icon,
+  const stockArticle = useCallback(
+    async (databaseId: string, articleProps: Article): Promise<Result<string>> => {
+      const { title, url, ogp, icon, tags, published } = articleProps;
+
+      const parameters: CreatePageParameters = {
+        parent: { database_id: databaseId },
+        properties: {
+          Title: {
+            type: "title",
+            title: [{ text: { content: title } }],
+          },
+          URL: { type: "url", url },
         },
       };
-    }
 
-    if (tags.length > 0) {
-      const relations = tags.map((tag) => ({ id: tag }));
-      parameters.properties["Tag"] = {
-        relation: relations,
-      };
-    }
-
-    if (published) {
-      parameters.properties["Published"] = {
-        type: "date",
-        date: { start: formatDate(published) },
-      };
-    }
-
-    if (ogp) {
-      parameters.cover = {
-        type: "external",
-        external: {
-          url: ogp,
-        },
-      };
-    }
-
-    try {
-      await client.pages.create(parameters);
-
-      return {
-        type: "success",
-        data: "ok",
-      };
-    } catch (err) {
-      if (err instanceof APIResponseError) {
-        return {
-          type: "failure",
-          err: {
-            name: err.name,
-            message: err.message,
+      if (icon) {
+        parameters.icon = {
+          type: "external",
+          external: {
+            url: icon,
           },
         };
       }
 
-      return {
-        type: "failure",
-        err: {
-          name: "create page error",
-          message: "failed to stock article",
-        },
-      };
-    }
-  };
+      if (tags.length > 0) {
+        const relations = tags.map((tag) => ({ id: tag }));
+        parameters.properties["Tag"] = {
+          relation: relations,
+        };
+      }
 
-  const fetchBookmarks = async (databaseId: string): Promise<readonly Bookmark[]> => {
-    const results = await fetchDatabase(databaseId);
-    if (!results[0]) {
-      return [];
-    }
-    const propertyIds = Object.entries(results[0].properties).map((property) => property[1].id);
+      if (published) {
+        parameters.properties["Published"] = {
+          type: "date",
+          date: { start: formatDate(published) },
+        };
+      }
 
-    const bookmarks = await Promise.all(
-      results.map(async (result) => {
-        const favicon = getIcon(result.icon);
-        const properties = await fetchProperties(result.id, propertyIds);
-        const bookmark = getBookmark(result.id, properties, favicon);
+      if (ogp) {
+        parameters.cover = {
+          type: "external",
+          external: {
+            url: ogp,
+          },
+        };
+      }
 
-        return bookmark;
-      })
-    );
+      try {
+        await client.pages.create(parameters);
 
-    return bookmarks;
-  };
+        return {
+          type: "success",
+          data: "ok",
+        };
+      } catch (err) {
+        if (err instanceof APIResponseError) {
+          return {
+            type: "failure",
+            err: {
+              name: err.name,
+              message: err.message,
+            },
+          };
+        }
+
+        return {
+          type: "failure",
+          err: {
+            name: "create page error",
+            message: "failed to stock article",
+          },
+        };
+      }
+    },
+    [client.pages]
+  );
+
+  const fetchBookmarks = useCallback(
+    async (databaseId: string): Promise<readonly Bookmark[]> => {
+      const results = await fetchDatabase(databaseId);
+      if (!results[0]) {
+        return [];
+      }
+      const propertyIds = Object.entries(results[0].properties).map((property) => property[1].id);
+
+      const bookmarks = await Promise.all(
+        results.map(async (result) => {
+          const favicon = getIcon(result.icon);
+          const properties = await fetchProperties(result.id, propertyIds);
+          const bookmark = getBookmark(result.id, properties, favicon);
+
+          return bookmark;
+        })
+      );
+
+      return bookmarks;
+    },
+    [fetchDatabase, fetchProperties, getBookmark, getIcon]
+  );
 
   return { fetchTags, fetchBookmarks, stockArticle };
 };
